e2cbf0a1bb367d2a05360b455642dd50889d7610
Eduardo Valle <evalle@outlook.com>
2021-02-10 09:43:26 -0500

    Update creartrigger.sql
    Se corrigio el formato generado automaticamente a DBO-1234-2021

--- creartrigger.sql
+++ creartrigger.sql
@@ -1,16 +1,9 @@
-DELIMITER $$
-
 CREATE TRIGGER actualizar_garantia
 BEFORE INSERT
 ON garantia_bo FOR EACH ROW
 BEGIN
-  --variable de garantia
   DECLARE numGarantia int;
-  DECLARE currentid int;
-  --Extraemos el ultimo valor de garantia
-  SELECT @currentid = id, @numGarantia = numgar FROM garantia_bo where LEN(numgar) > 0 AND MAX(id)
-  --Actualizamos el registro actual
-  UPDATE garantia_bo SET numgar = "BCO-" + numGarantia + "2021" WHERE id = @currentid;
-END $$
-
-DELIMITER;
+  DECLARE currentid int;   
+  SELECT id,  CONVERT(SUBSTRING_INDEX(SUBSTRING_INDEX(numgar, '-', -2), '-', 1), UNSIGNED) + 1 INTO currentid, numGarantia FROM garantia_bo WHERE LENGTH(numgar) > 0 ORDER BY id DESC LIMIT 0, 1;
+  SET NEW.numgar = CONCAT( "BDO-", CONVERT(numGarantia, CHAR),"-", DATE_FORMAT(CURDATE(), '%Y') );
+END;
